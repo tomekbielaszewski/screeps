d290000e3804f6f4e5670f864e42683dd4040ab9
Grizwold <tomekbielaszewski@gmail.com>
2016-02-16 19:54:43 +0100

    cheatsheet update

--- scripts/cheatsheet.js
+++ scripts/cheatsheet.js
@@ -1,5 +1,6 @@
-Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Worker1' );
-Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder1' );
-Memory.creeps.Worker1.role = 'harvester';
-Memory.creeps.Builder1.role = 'builder';
-Game.spawns.Spawn1.createCreep( [TOUGH, ATTACK, MOVE, MOVE], 'Guard1', { role: 'guard' } );
+
+Memory.creeps.Worker1.type = 'harvester';
+Memory.creeps.Builder1.type = 'builder';
+Game.spawns[0].createCreep( [TOUGH, ATTACK, MOVE, MOVE], 'Guard1', { type: 'guardian' } );
+Game.spawns[0].createCreep( [WORK, CARRY, MOVE], 'Worker1', { type: 'harvester' } );
+Game.spawns[0].createCreep( [WORK, WORK, CARRY, MOVE], 'Builder1', { type: 'builder' } );
