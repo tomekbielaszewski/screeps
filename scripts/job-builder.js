41b18c83f26d468fed4997bded4ad5224abea72d
Grizwold <tomekbielaszewski@gmail.com>
2016-02-25 21:24:46 +0100

    Builder is optimized with 'stuck on path' validation + refactor of common functions

--- scripts/job-builder.js
+++ scripts/job-builder.js
@@ -1,15 +1,15 @@
+var service = require('service');
+
 module.exports = (function () {
+
   function work(creep) {
     if (canBuild(creep)) {
-
-      //console.log('canBuild');
-      var construction = findConstructionSite(creep);
+      var construction = service.find.withCache.constructionSite(creep);
       build(construction, creep);
     } else {
-      //console.log('cannotBuild');
-      var spawn = findStorage(creep)
+      var spawn = service.find.withCache.storage(creep);
       if (spawn.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
-        goTo(spawn.pos, creep);
+        service.goTo(spawn.pos, creep);
       }
     }
   }
@@ -18,46 +18,18 @@ module.exports = (function () {
     return creep.carry.energy > 0;
   }
 
-  function findConstructionSite(creep) {
-    return findStructure(creep, FIND_MY_CONSTRUCTION_SITES);
-  }
-
-  function findStorage(creep) {
-    return findStructure(creep, FIND_MY_STRUCTURES, STRUCTURE_STORAGE);
-  }
-
-  function findStructure(creep, finder, structureType) {
-    if(!(creep.memory.structure && creep.memory.structure.structureType == structureType)) {
-      console.log("finding structure... " + structureType);
-      creep.memory.structure = creep.pos.findClosestByRange(finder, {
-        filter: function (s) {
-          return structureType ? s.structureType == structureType : true;
-        }
-      });
-    }
-    return Game.getObjectById(creep.memory.structure.id);
-  }
-
-  function goTo(target, creep) {
-    if(!(JSON.stringify(target) === JSON.stringify(creep.memory.target))) {
-      creep.memory.target = target;
-      creep.memory.path = creep.pos.findPathTo(target);
-    }
-    creep.moveByPath(creep.memory.path);
-  }
-
   function build(construction, creep) {
     var buildResult = creep.build(construction);
 
     if (buildResult == ERR_NOT_IN_RANGE) {
-      goTo(construction.pos, creep);
+      service.goTo(construction.pos, creep);
     }
 
     if (buildResult == ERR_RCL_NOT_ENOUGH) {
       creep.say('RCL to low');
     }
     if (buildResult == ERR_NO_BODYPART) {
-      creep.say('no WORK');
+      creep.say('no [WORK]');
     }
   }
 
---
+++ scripts/service.js
@@ -0,0 +1,82 @@
+module.exports = (function () {
+
+  function findStorage(creep) {
+    if (creep.memory.structure && creep.memory.structure.structureType == STRUCTURE_STORAGE) {
+      return Game.getObjectById(creep.memory.structure.id);
+    }
+    return findStructureType(creep, FIND_MY_STRUCTURES, STRUCTURE_STORAGE);
+  }
+
+  function findConstructionSite(creep) {
+    if (creep.memory.structure) {
+      var construction = Game.getObjectById(creep.memory.structure.id);
+      if (construction instanceof ConstructionSite && (construction.progress < construction.progressTotal)) {
+        return construction;
+      }
+    }
+    return findStructure(creep, FIND_MY_CONSTRUCTION_SITES);
+  }
+
+  function findStructureType(creep, finder, structureType) {
+    var filter = function (s) {
+      return structureType ? s.structureType == structureType : true;
+    };
+    return findStructure(creep, finder, filter);
+  }
+
+  function findStructure(creep, finder, filter) {
+    var structure = creep.pos.findClosestByRange(finder, {
+      filter: filter
+    });
+    creep.memory.structure = structure;
+    return structure;
+  }
+
+  function goTo(target, creep) {
+    if (!target instanceof RoomPosition) {
+      throw 'Given target is not an instance of RoomPosition!'
+    }
+    if (!(isEqual(target, creep.memory.target)) || stuck(creep)) {
+      console.log(creep.name + ' pathfind..');
+      creep.memory.target = target;
+      creep.memory.path = creep.pos.findPathTo(target);
+    }
+    creep.moveByPath(creep.memory.path);
+  }
+
+  function stuck(creep) {
+    if(!creep.spawning) {
+      var lastPos = _.clone(creep.memory.lastPos);
+      creep.memory.lastPos = creep.pos;
+
+      if (isEqual(creep.pos, lastPos)) {
+        creep.memory.stuckTicks = creep.memory.stuckTicks + 1;
+        if (creep.memory.stuckTicks > 3) {
+          console.log(creep.name + ' stuck!');
+          return true;
+        } else {
+          return false;
+        }
+      }
+      creep.memory.stuckTicks = 0;
+    } else {
+      return false;
+    }
+  }
+
+  function isEqual(o1, o2) {
+    return (JSON.stringify(o1) === JSON.stringify(o2));
+  }
+
+  return {
+    find: {
+      byStructure: findStructure,
+      byStructureType: findStructureType,
+      withCache: {
+        storage: findStorage,
+        constructionSite: findConstructionSite
+      }
+    },
+    goTo: goTo
+  }
+}());
