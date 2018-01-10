a65b2c0072257f1219aadcd39a711d4168d32982
Grizwold <tomekbielaszewski@gmail.com>
2018-01-10 19:29:53 +0100

    refactored scheduler for running action every x ticks

--- scripts/main.js
+++ scripts/main.js
@@ -10,19 +10,26 @@ require('./spawn__unit_creation_queue');
 const unitManagerUpdate = require('./manager__unit_creator');
 
 module.exports.loop = function () {
-    every(100, unitManagerUpdate);
+    every(100).ticks.run(unitManagerUpdate);
 
     _.forEach(Game.creeps, creep => {
         let role = creep.memory.role;
         creep[role]();
     });
+
     _.forEach(Game.spawns, spawn => {
         spawn.processQueue();
     });
 };
 
-function every(ticks, fn) {
-    if(Game.time % ticks === 0) {
-        fn();
-    }
+function every(ticks) {
+    return {
+        ticks: {
+            run: function(fn) {
+                if(Game.time % ticks === 0) {
+                    fn();
+                }
+            }
+        }
+    };
 }
