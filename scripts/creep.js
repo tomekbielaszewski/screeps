d80c11a657b58c460f727c5de58642aaade5209f
Grizwold <tomekbielaszewski@gmail.com>
2018-01-13 01:56:24 +0100

    helper methods in creep

--- scripts/creep.js
+++ scripts/creep.js
@@ -8,3 +8,11 @@ const _ = require('lodash');
 Creep.prototype.isCapacityFull = function () {
     return _.sum(this.carry) >= this.carryCapacity;
 };
+
+Creep.prototype.isCarryingSomething = function () {
+    return _.sum(this.carry) > 0;
+};
+
+Creep.prototype.isCarrying = function (resourceType) {
+    return this.carry[resourceType] > 0;
+};
