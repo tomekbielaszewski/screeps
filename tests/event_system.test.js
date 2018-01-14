91de64fb016b7e88c476eef43a8e955cb8a8a770
Grizwold <tomekbielaszewski@gmail.com>
2018-01-14 01:17:16 +0100

    event system tests

---
+++ tests/event_system.test.js
@@ -0,0 +1,30 @@
+global.Memory = {};
+
+const eventSystem = require('../scripts/event_system');
+
+test('Publishing adds event to Memory', () => {
+    let event = {some:'thing'};
+    eventSystem.publish(event);
+
+    expect(Memory.events).toBeDefined();
+    expect(Memory.events.length).toBe(1);
+    expect(Memory.events[0]).toBe(event);
+});
+
+test('Publishing again adds second event to Memory', () => {
+    let event = {some:'thingelse'};
+    eventSystem.publish(event);
+
+    expect(Memory.events.length).toBe(2);
+    expect(Memory.events[1]).toBe(event);
+});
+
+test('Getting first event removes it from Memory', () => {
+    let event = {some:'thing'};
+    let result = eventSystem.get((e) => {
+        return e.some === event.some;
+    });
+
+    expect(Memory.events.length).toBe(1);
+    expect(result).toEqual(event);
+});
