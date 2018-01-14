d4727724d0c26d54a77b895b6045ab98b65a467d
Grizwold <tomekbielaszewski@gmail.com>
2018-01-14 21:32:54 +0100

    event system logging

--- scripts/event_system.js
+++ scripts/event_system.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const log = require('loggers');
 
 function EventSystem() {
     Memory.events = Memory.events || [];
@@ -7,13 +8,18 @@ function EventSystem() {
 EventSystem.prototype = {
     publish: function (event) {
         Memory.events.push(event);
+        this.log(`Event added. Event bus size: ${Memory.events.length}`);
     },
     get: function (filter) {
         let event = _(Memory.events)
             .filter(filter)
             .first();
         _.remove(Memory.events, e => e === event);
+        this.log(`Event ${event.type} removed. Event bus size: ${Memory.events.length}`);
         return event;
+    },
+    log: function (message) {
+        log('EventSystem', message);
     }
 };
 
