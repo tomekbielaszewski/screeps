9532c9c69c1ffceec3ef54a0e123f54e9437cf42
Grizwold <tomekbielaszewski@gmail.com>
2018-01-14 01:19:11 +0100

    upgrader job with some try outs of event system

--- scripts/creep__job_upgrader.js
+++ scripts/creep__job_upgrader.js
@@ -1,3 +1,29 @@
 Creep.prototype[ROLE_UPGRADER] = function () {
-    // console.log("Upgrader upgrading");
+    let controller = this.room.controller;
+
+    if(this.pos.isNearTo(controller)) {
+        upgrade.call(this, controller);
+    } else {
+        this.moveTo(controller.pos);
+    }
 };
+
+function upgrade(controller) {
+    if(this.isCarrying(RESOURCE_ENERGY)) {
+        this.upgradeController(controller);
+    } else {
+        let resource = this.pos.findDroppedResource();
+        if (this.pos.isNearTo(resource.pos)) {
+            this.pickup(resource);
+            this.memory.eventSent = false;
+        } else {
+            if(!this.memory.eventSent) {
+                Memory.events.push({
+                    type: EVENT__TRANSPORT_RESOURCES,
+                    target: this.pos
+                });
+                this.memory.eventSent = true;
+            }
+        }
+    }
+}
