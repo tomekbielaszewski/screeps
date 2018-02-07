9330371df4fcc411a253f1a9d1ec4a0636f68213
tbielaszewski <tomekbielaszewski@gmail.com>
2018-02-07 13:57:06 +0100

    Worker: hiring carrier to collect energy

--- scripts/creep__job_worker.js
+++ scripts/creep__job_worker.js
@@ -1,10 +1,37 @@
+const eventSystem = require('./event_system');
+
+const states = [
+    hireCarrierToCollectResources,
+    work
+];
+
 Creep.prototype[ROLE_WORKER] = {
     onSpawn: function () {
     },
     onDie: function () {
     },
     work: function () {
-        let source = this.pos.findSource();
-        this.harvestOrMoveTo(source)
+        states[this.memory.state].call(this);
     }
 };
+
+function work() {
+    let source = this.pos.findSource();
+    this.harvestOrMoveTo(source)
+}
+
+function hireCarrierToCollectResources() {
+    eventSystem.publish({
+        type: EVENT__HIRE_TO_COLLECT_ENERGY,
+        target: this.id
+    });
+    setState.call(this, 1, 'Carrier hired for collecting energy');
+}
+
+function setState(state, message) {
+    if (message) {
+        const oldState = this.memory.state;
+        this.log(`${message}. Advancing from state ${oldState} to ${state}`);
+    }
+    this.memory.state = state;
+}
--- scripts/enums__game_events.js
+++ scripts/enums__game_events.js
@@ -1,1 +1,2 @@
 global.EVENT__HIRE_TO_TRANSPORTING_ENERGY = 'hireToTransportingResources';
+global.EVENT__HIRE_TO_COLLECT_ENERGY = 'hireToCollectResources';
